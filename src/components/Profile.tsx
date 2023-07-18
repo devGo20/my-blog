8f41f1c0ee14803441b9a07b65b021f48cec3710
devGo20 <devbin20@gmail.com>
2023-07-18 16:56:45 +0900

    feat: Profile component create and page add

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+export default function Profile() {
+  return (
+    <section className='flex items-center flex-col'>
+      <Image
+        src={`/images/profile.jpeg`}
+        alt='profile'
+        width={150}
+        height={150}
+        className='rounded-full'
+      />
+      <h1 className='font-bold text-lg'>HI, I'm eunbin</h1>
+      <p className='text-base'>Front engineer</p>
+      <span className='text-xs text-gray-600'>풀스택 개발자가 되고픈 은빈</span>
+      <button className='text-xs rounded-full bg-yellow-300 px-4 py-2 font-bold mt-2'>
+        Contact Me!
+      </button>
+    </section>
+  );
+}
