0bb197dc58a0a08b4c110285e177bb9435ff1fa5
devGo20 <devbin20@gmail.com>
2023-07-24 17:00:53 +0900

    feat: PostPage > FilterPosts component add

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,7 @@
-export default function PostsPage() {
-  return <h1>PostsPage</h1>;
+import FilterPosts from '@/components/FilterPosts';
+import { getPosts } from '@/service/posts';
+
+export default async function PostsPage() {
+  const { posts, uniqueCategories } = await getPosts();
+  return <FilterPosts posts={posts} categories={uniqueCategories} />;
 }
