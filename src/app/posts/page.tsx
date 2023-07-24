5965b695b01092859bb2fb9b980669b70004b5d1
devGo20 <devbin20@gmail.com>
2023-07-24 17:41:50 +0900

    fix: PostsPage > categories  update

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,8 @@
 import FilterPosts from '@/components/FilterPosts';
-import { getPosts } from '@/service/posts';
+import { getAllPosts } from '@/service/posts';
 
 export default async function PostsPage() {
-  const { posts, uniqueCategories } = await getPosts();
-  return <FilterPosts posts={posts} categories={uniqueCategories} />;
+  const posts = await getAllPosts();
+  const categories = [...new Set(posts.map((post) => post.category))];
+  return <FilterPosts posts={posts} categories={categories} />;
 }
