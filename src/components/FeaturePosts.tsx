871582ff54bbb6dc31937670b5e28050a7e973e6
devGo20 <devbin20@gmail.com>
2023-07-24 17:01:15 +0900

    feat: FeaturePosts component create

--- src/components/FeaturePosts.tsx
+++ src/components/FeaturePosts.tsx
@@ -4,9 +4,12 @@ import PostCard from './PostCard';
 export default async function FeaturePosts() {
   const featuredPosts = await getIsFeaturedPosts(true);
   return (
-    <section className='mb-10'>
+    <section className='mb-10 '>
       <h1 className='font-bold mb-3'>Featured Posts</h1>
-      <div className='grid grid-cols-3 gap-4'>
+      <div
+        className='flex-wrap justify-center sm:grid md:grid-cols-2 xl:grid-cols-3 
+        3xl:flex gap-4'
+      >
         {featuredPosts.map((post, index) => (
           <PostCard key={index} {...post} />
         ))}
